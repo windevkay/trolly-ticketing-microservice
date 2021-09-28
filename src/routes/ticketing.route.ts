a591167c4a5d1a27565f36a75a52ef922d9fef66
Kola Adebayo <kolaadebayo@Kolas-MacBook-Pro.local>
2021-09-28 17:33:58 -0600

    completed ticket routes

--- src/routes/ticketing.route.ts
+++ src/routes/ticketing.route.ts
@@ -4,6 +4,7 @@ import {
   requireAuth,
   validateRequest,
   NotFoundError,
+  NotAuthorizedError,
 } from "@stagefirelabs/common";
 
 import { Ticket } from "../models/ticket.model";
@@ -46,5 +47,34 @@ router.get("/", async (req: Request, res: Response) => {
   const tickets = await Ticket.find({});
   res.send(tickets);
 });
+
+router.put(
+  "/:id",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price should be greater than zero"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const ticket = await Ticket.findById(id).exec();
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+    if (ticket.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    ticket.set({
+      title: req.body.title,
+      price: req.body.price,
+    });
+    await ticket.save();
+
+    res.send(ticket);
+  }
+);
 
 export { router as ticketingRouter };
